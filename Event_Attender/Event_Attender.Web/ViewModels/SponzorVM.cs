d75bb1cac1391f772007af55e265bd7f0403b4fa
Zinedin Mezit <zinedin.mezit@edu.fit.ba>
2019-11-18 19:07:13 +0100

    Prikaz i dodavanje sponzora

---
+++ Event_Attender/Event_Attender.Web/ViewModels/SponzorVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Event_Attender.Web.ViewModels
+{
+    public class SponzorVM
+    {
+        public string Naziv { get; set; }
+        public string Telefon { get; set; }
+        public string Email { get; set; }
+    }
+}
