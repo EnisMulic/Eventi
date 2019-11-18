107566a035a65f3ccf6521f64b9cd2ca575d8da8
Azra Becirevic <azra.becirevic1998@gmail.com>
2019-11-18 17:12:29 +0100

    Upravljenje gradovima i drzavama

--- Event_Attender/Event_Attender.Web/Controllers/DrzavaController.cs
+++ Event_Attender/Event_Attender.Web/Controllers/DrzavaController.cs
@@ -16,9 +16,60 @@ namespace Event_Attender.Web.Controllers
             {
                 List<Drzava> drzave = ctx.Drzava.ToList();
                 ViewData["drzave"] = drzave;
-                // nece ispisati drzave jer je baza prazna
             }
             return View();
+        }
+        public IActionResult DodajForma(int poziv, int gradId)
+        {
+            ViewData["drzava"] = new Drzava();
+            ViewData["poziv"] = poziv;
+            ViewData["gradId"] = gradId;
+            return View();
+        }
+        public IActionResult Uredi(int id)
+        {
+            using(MojContext ctx= new MojContext())
+            {
+                ViewData["drzava"]= ctx.Drzava.Find(id);
+                ViewData["poziv"] = 0;
+                ViewData["gradId"] = 0;
+            }
+            return View("DodajForma");
+        }
+        public IActionResult Snimi(int id,string naziv, int poziv, int gradId)
+        {
+            MojContext ctx = new MojContext();
+            Drzava d;
+            if (id == 0)
+            {
+                // dodaje se nova drzava
+                d = new Drzava();
+                ctx.Drzava.Add(d);
+            }
+            else
+            {
+                d = ctx.Drzava.Find(id);
+            }
+            d.Naziv = naziv;
+           
+            ctx.SaveChanges();
+            if (poziv == 2)
+            {   if (gradId != 0)
+                    return Redirect("/Grad/Uredi?id=" + gradId);
+                return Redirect("/Grad/Dodaj");
+            }
+            return Redirect("/Drzava/Index");
+           
+        }
+        public IActionResult Obrisi(int id)
+        {   using(MojContext ctx=new MojContext())
+            {
+                Drzava d = ctx.Drzava.Find(id);
+                ctx.Drzava.Remove(d);
+                ctx.SaveChanges();
+            }
+         
+            return Redirect("/Drzava/Index");
         }
     }
 }
---
+++ Event_Attender/Event_Attender.Web/Controllers/GradController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventAttender.Data.EF;
+using EventAttender.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Event_Attender.Web.Controllers
+{
+    public class GradController : Controller
+    {
+        public IActionResult Index()
+        {
+            using(MojContext ctx=new MojContext())
+            {
+                List<Grad> gradovi = ctx.Grad.Include(g=>g.Drzava).ToList();
+                ViewData["gradovi"] = gradovi;
+            }
+            return View();
+        }
+        public IActionResult Dodaj()
+        {   
+            ViewData["grad"] = new Grad();
+            using(MojContext ctx=new MojContext())
+            {
+               ViewData["drzave"]= ctx.Drzava.ToList();
+            }
+            return View();
+        }
+        public IActionResult Snimi(int id, int drzavaId, string naziv)
+        {
+            Grad g;
+            MojContext ctx = new MojContext();
+            if (id == 0)
+            {
+                g= new Grad();
+                ctx.Grad.Add(g);
+            }
+            else
+            {
+                g = ctx.Grad.Find(id);
+            }
+           
+            g.Naziv = naziv;
+            g.DrzavaId = drzavaId;
+          
+            ctx.SaveChanges();
+            return Redirect("/Grad/Index");
+        }
+        public IActionResult Obrisi(int id)
+        {
+            using(MojContext ctx=new MojContext())
+            {
+                Grad g = ctx.Grad.Find(id);
+                ctx.Grad.Remove(g);
+                ctx.SaveChanges();
+            }
+            return Redirect("/Grad/Index");
+        }
+        public IActionResult Uredi(int id)
+        {
+            using (MojContext ctx = new MojContext())
+            {
+                ViewData["grad"] =  ctx.Grad.Find(id);
+                ViewData["drzave"] = ctx.Drzava.ToList();
+            }
+            return View("Dodaj");
+        }
+    }
+}
