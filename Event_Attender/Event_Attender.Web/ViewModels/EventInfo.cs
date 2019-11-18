4e59ac03527d3701c7caa847d9def8430d24d24a
Enis Mulić <enis.mulic@edu.fit.ba>
2019-11-18 22:41:49 +0100

    Odobravanje Eventa
        - [Add] Odobravanje eventa i uklanjanje sa liste ne odobrenih
        - [Add] ViewModel - EventInfo
        - [Change] _Layout: Naziv applikacije iz Event_Attender.Web u Event Event_Attender

---
+++ Event_Attender/Event_Attender.Web/ViewModels/EventInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Event_Attender.Web.ViewModels
+{
+    public class EventInfo
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+        public DateTime DatumOdrzavanja { get; set; }
+        public string VrijemeOdrzavanja { get; set; }
+        public Kategorija Kategorija { get; set; }
+        public bool IsOdobren { get; set; }
+        public bool IsOtkazan { get; set; }
+        public string OrganizatorNaziv { get; set; }
+        public string ProstorOdrzavanjaNaziv { get; set; }
+    }
+}
