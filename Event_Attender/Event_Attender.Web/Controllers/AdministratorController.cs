f3e88b779665eb206e0e7c1e8bbcc34acc6c6da9
Enis Mulić <enis.mulic@edu.fit.ba>
2019-11-19 14:18:34 +0100

    Add hardcoded fallback test for AdministratorController

--- Event_Attender/Event_Attender.Web/Controllers/AdministratorController.cs
+++ Event_Attender/Event_Attender.Web/Controllers/AdministratorController.cs
@@ -15,6 +15,7 @@ namespace Event_Attender.Web.Controllers
         
         public IActionResult Index()
         {
+            Test_HardCode_DodajEvent(3);
             using (MojContext ctx = new MojContext())
             {
                 List<EventInfo> events = ctx.Event
@@ -54,7 +55,81 @@ namespace Event_Attender.Web.Controllers
             return Redirect("/Administrator");
         }
 
-        
+        public void Test_HardCode_DodajEvent(int n)
+        {
+            
+            using MojContext ctx = new MojContext();
+            if (ctx.Organizator.Count() == 0)
+            {
+                List<Organizator> organizatori = new List<Organizator>
+                    {
+                        new Organizator { Naziv = "Organizator 1"},
+                        new Organizator { Naziv = "Organizator 2"},
+                        new Organizator { Naziv = "Organizator 3"}
+                    };
+                ctx.Organizator.AddRange(organizatori);
+                ctx.SaveChanges();
+            }
+
+            if (ctx.ProstorOdrzavanja.Count() == 0)
+            {
+                if (ctx.Grad.Count() == 0)
+                {
+                    ctx.Drzava.Add(new Drzava { Naziv = "Burkina Faso" });
+                    ctx.SaveChanges();
+
+                    Drzava tempDrzava = ctx.Drzava
+                        .Where(d => d.Naziv == "Burkina Faso")
+                        .FirstOrDefault();
+                    ctx.Grad.Add(new Grad { Naziv = "Ouagadougou", DrzavaId = tempDrzava.Id});
+                    ctx.SaveChanges();
+                }
+
+                Grad tempGrad = ctx.Grad.FirstOrDefault();
+                List<ProstorOdrzavanja> prostoriOdrzavanja = new List<ProstorOdrzavanja>
+                    {
+                        new ProstorOdrzavanja {Naziv = "Prostor 1", GradId = tempGrad.Id},
+                        new ProstorOdrzavanja {Naziv = "Prostor 2", GradId = tempGrad.Id}
+                    };
+                ctx.ProstorOdrzavanja.AddRange(prostoriOdrzavanja);
+                ctx.SaveChanges();
+            }
+
+
+            if (ctx.Event.Where(e => e.IsOdobren == false).Count() == 0)
+            {
+                List<Event> events = new List<Event>();
+                
+                List<Organizator> organizatori = ctx.Organizator.ToList();
+                Random rndOrg = new Random();
+
+                List<ProstorOdrzavanja> prostori = ctx.ProstorOdrzavanja.ToList();
+                Random rndProstor = new Random();
+
+                Random rndEvent = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    
+                    events.Add
+                    (
+                        new Event
+                        {
+                            Naziv = "Event Test " + rndEvent.Next(),
+                            DatumOdrzavanja = Convert.ToDateTime("31.12.2019"),
+                            VrijemeOdrzavanja = "20:00",
+                            Kategorija = (Kategorija)(i % 2 + 1),
+                            IsOdobren = false,
+                            IsOtkazan = false,
+                            OrganizatorId = organizatori[rndOrg.Next(0, organizatori.Count() - 1)].Id,
+                            ProstorOdrzavanjaId = prostori[rndProstor.Next(0, prostori.Count() - 1)].Id
+                        }
+                    );
+                }
+                ctx.AddRange(events);
+
+                ctx.SaveChanges();
+            }
+        }
   
     }
 }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
