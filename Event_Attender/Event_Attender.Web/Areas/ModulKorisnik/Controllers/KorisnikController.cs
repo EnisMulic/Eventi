62ec8220342266c8843a11fa27f616fc42561f82
Azra <azra.becirevic@edu.fit.ba>
2019-12-18 16:51:37 +0100

    Pretraga po kategoriji, nazivi lokaciji TaskID 36,37, UserstoryId 33

--- Event_Attender/Event_Attender.Web/Areas/ModulKorisnik/Controllers/KorisnikController.cs
+++ Event_Attender/Event_Attender.Web/Areas/ModulKorisnik/Controllers/KorisnikController.cs
@@ -21,7 +21,7 @@ namespace Event_Attender.Web.Areas.ModulKorisnik.Controllers
         //{
         //    ctx = context;
         //}
-        public IActionResult Index()
+        public IActionResult Index(string filter)
         {   
             MojContext ctx = new MojContext();
 
@@ -44,10 +44,38 @@ namespace Event_Attender.Web.Areas.ModulKorisnik.Controllers
 
             
             PretragaEventaVM model = new PretragaEventaVM();
-           
             DateTime date = DateTime.Now;
 
-            model.Eventi = ctx.Event.Include(e => e.ProstorOdrzavanja).Include(e => e.ProstorOdrzavanja.Grad)
+            if (filter == "Muzika")
+            {
+                model.Eventi = PretragaPoKategoriji(Kategorija.Muzika);
+                return View(model);
+            }
+            else if (filter == "Sport")
+            {
+                model.Eventi = PretragaPoKategoriji(Kategorija.Sport);
+                return View(model);
+            }
+            else if(filter == "Kultura")
+            {
+                model.Eventi = PretragaPoKategoriji(Kategorija.Kultura);
+                return View(model);
+            }
+            if (filter != null)
+            {   
+                model.Eventi = PretragaPoNazivuLokaciji(filter);
+                return View(model);
+            }
+            
+            model.Eventi = PrikazEvenata();
+
+            return View(model);  
+        }
+        List<PretragaEventaVM.Rows> PrikazEvenata()
+        {
+            DateTime date = DateTime.Now;
+            MojContext ctx = new MojContext();
+            List<PretragaEventaVM.Rows> lista= ctx.Event/*.Include(e => e.ProstorOdrzavanja).Include(e => e.ProstorOdrzavanja.Grad)*/
                 .Where(e => e.IsOdobren == true).Where(e => e.IsOtkazan == false).Where(e => e.DatumOdrzavanja.CompareTo(date) == 1)
                 .Select(e => new PretragaEventaVM.Rows
                 {
@@ -62,10 +90,54 @@ namespace Event_Attender.Web.Areas.ModulKorisnik.Controllers
                     VrijemeOdrzavanja = e.VrijemeOdrzavanja,
                     Slika = e.Slika
                 }).ToList();
-
-            return View(model);  
+            return lista;
         }
-
+        List<PretragaEventaVM.Rows> PretragaPoKategoriji(Kategorija k)
+        {
+            DateTime date = DateTime.Now;
+            MojContext ctx = new MojContext();
+            List<PretragaEventaVM.Rows> lista = ctx.Event/*.Include(e => e.ProstorOdrzavanja).Include(e => e.ProstorOdrzavanja.Grad)*/
+            .Where(e => e.Kategorija == k).Where(e => e.IsOdobren == true).Where(e => e.IsOtkazan == false).Where(e => e.DatumOdrzavanja.CompareTo(date) == 1)
+            .Select(e => new PretragaEventaVM.Rows
+            {
+                EventId = e.Id,
+                Naziv = e.Naziv,
+                Kategorija = e.Kategorija.ToString(),
+                Opis = e.Opis,
+                ProstorOdrzavanjaNaziv = e.ProstorOdrzavanja.Naziv,
+                ProstorOdrzavanjaAdresa = e.ProstorOdrzavanja.Adresa,
+                ProstorOdrzavanjaGrad = e.ProstorOdrzavanja.Grad.Naziv,
+                DatumOdrzavanja = e.DatumOdrzavanja.Day.ToString() + "." + e.DatumOdrzavanja.Month.ToString() + "." + e.DatumOdrzavanja.Year.ToString(),
+                VrijemeOdrzavanja = e.VrijemeOdrzavanja,
+                Slika = e.Slika
+            }).ToList();
+            return lista;
+        }
+        List<PretragaEventaVM.Rows> PretragaPoNazivuLokaciji(string filter)
+        {
+            DateTime date = DateTime.Now;
+            MojContext ctx = new MojContext();
+            List<PretragaEventaVM.Rows> lista= ctx.Event.Where(e => e.IsOdobren == true)
+                .Where(e => e.IsOtkazan == false).Where(e => e.DatumOdrzavanja.CompareTo(date) == 1)
+                .Where(e => e.Naziv.ToLower().StartsWith(filter.ToLower())
+                       || e.Naziv.ToLower().Contains(filter.ToLower()) ||
+                        e.ProstorOdrzavanja.Naziv.ToLower().StartsWith(filter.ToLower()) || e.ProstorOdrzavanja.Naziv.ToLower().Contains(filter.ToLower())
+                       || e.ProstorOdrzavanja.Grad.Naziv.ToLower().StartsWith(filter.ToLower()) || e.ProstorOdrzavanja.Grad.Naziv.ToLower().Contains(filter.ToLower()))
+                      .Select(e => new PretragaEventaVM.Rows
+                      {
+                          EventId = e.Id,
+                          Naziv = e.Naziv,
+                          Kategorija = e.Kategorija.ToString(),
+                          Opis = e.Opis,
+                          ProstorOdrzavanjaNaziv = e.ProstorOdrzavanja.Naziv,
+                          ProstorOdrzavanjaAdresa = e.ProstorOdrzavanja.Adresa,
+                          ProstorOdrzavanjaGrad = e.ProstorOdrzavanja.Grad.Naziv,
+                          DatumOdrzavanja = e.DatumOdrzavanja.Day.ToString() + "." + e.DatumOdrzavanja.Month.ToString() + "." + e.DatumOdrzavanja.Year.ToString(),
+                          VrijemeOdrzavanja = e.VrijemeOdrzavanja,
+                          Slika = e.Slika
+                      }).ToList();
+            return lista;
+        }
         public string OEventu()
         {
             return "Event";
