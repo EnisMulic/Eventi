681a3879b8026a9d4933208fc5d6e8f767de6d81
Enis Mulić <enis.mulic@edu.fit.ba>
2019-12-23 19:03:14 +0100

    [Add] Organizator Dodaj 	UserStory ID 40; Task ID 44

--- Event_Attender/Event_Attender.Web/Areas/Administrator/Models/OrganizatorVM.cs
+++ Event_Attender/Event_Attender.Web/Areas/Administrator/Models/OrganizatorVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,7 +10,14 @@ namespace Event_Attender.Web.Areas.Administrator.Models
     {
         public int Id { get; set; }
         public string Naziv { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
         public string Telefon { get; set; }
+        public int GradId { get; set; }
         public string GradNaziv { get; set; }
+
+        public List<SelectListItem> Gradovi { get; set; }
     }
 }
