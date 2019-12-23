9ea610b56ab3dd664bbbc9018c17cf0e9b7613da
Enis Mulić <enis.mulic@edu.fit.ba>
2019-12-23 19:04:59 +0100

    [Add] Radnik Dodaj 	UserStoryID 40; TaskID 43

--- Event_Attender/Event_Attender.Web/Areas/Administrator/Models/RadnikVM.cs
+++ Event_Attender/Event_Attender.Web/Areas/Administrator/Models/RadnikVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +11,17 @@ namespace Event_Attender.Web.Areas.Administrator.Models
         public int Id { get; set; }
         public string Ime { get; set; }
         public string Prezime { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
         public string Telefon { get; set; }
+        public int GradId { get; set; }
         public string GradNaziv { get; set; }
+        public List<SelectListItem> Gradovi { get; set; }
+
+        public string ImePrezime
+        {
+            get { return Ime + " " + Prezime; }
+        }
     }
 }
