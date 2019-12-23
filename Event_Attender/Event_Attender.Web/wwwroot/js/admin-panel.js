b0b544053be56b04b0ba79d88818057092673954
Enis Mulić <enis.mulic@edu.fit.ba>
2019-12-23 19:13:22 +0100

    [Remove] Old Files [Edit] HomeController for new file structure

--- Event_Attender/Event_Attender.Web/wwwroot/js/admin-panel.js
+++ Event_Attender/Event_Attender.Web/wwwroot/js/admin-panel.js
@@ -1,7 +1,42 @@
 ﻿$(document).ready(function () {
-    console.log("ready!");
 
-    $("tr").click(function () {
-        alert("Handler for .click() called.");
+    $("div.ea-navbar").on('click', function (e) {
+        $.get("../Home/" + e.target.id + "List", function (response) {
+            $("#Main-Display").html(response);
+
+            SetActionsInfo(e.target.id);
+            SetActionsUredi(e.target.id);
+            SetActionDodaj(e.target.id);
+        });
     });
-});
+
+    $("div.ea-navbar-home").on('click', function (e) {
+        $("#Main-Display").html("");
+    });
+
+    function SetActionsInfo(type) {
+        $("button[for=\"Info\"]").on('click', function (e) {
+            $.get("../Home/" + type + "Info?id=" + e.target.id, function (response) {
+                $("#Main-Display").html(response);
+
+                SetActionsUredi(type);
+            });
+        });
+    }
+
+    function SetActionsUredi(type) {
+        $("button[for=\"Uredi\"]").on('click', function (e) {
+            $.get("../Home/" + type + "Uredi?id=" + e.target.id, function (response) {
+                $("#Main-Display").html(response);
+            });
+        });
+    }
+
+    function SetActionDodaj(type) {
+        $("button[for=\"Dodaj\"]").on('click', function (e) {
+            $.get("../Home/" + type + "Dodaj", function (response) {
+                $("#Main-Display").html(response);
+            });
+        });
+    }
+})
