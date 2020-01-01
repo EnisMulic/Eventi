9fa29c111a3715257f8f0d24a5af611732c90922
Enis Mulić <enis.mulic@edu.fit.ba>
2020-01-01 15:28:40 +0100

    [Add] Autorization for Admin

--- Event_Attender/Event_Attender.Web/Areas/Administrator/Models/AdministratorVM.cs
+++ Event_Attender/Event_Attender.Web/Areas/Administrator/Models/AdministratorVM.cs
@@ -11,6 +11,10 @@ namespace Event_Attender.Web.Areas.Administrator.Models
         public string Ime { get; set; }
         public string Prezime { get; set; }
         public string Telefon { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
         public string Grad { get; set; }
+        public int LogPodaciId { get; set; }
     }
 }
