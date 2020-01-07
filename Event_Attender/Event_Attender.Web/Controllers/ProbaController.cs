1ee81852fa99be5b6f57f48e2d58637380ccbdcd
Adil <adil@fit.ba>
2020-01-07 09:04:39 +0100

    proba nastava

---
+++ Event_Attender/Event_Attender.Web/Controllers/ProbaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Event_Attender.Web.Controllers
+{
+    public class ProbaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
