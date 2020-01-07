a36120264b97e1c1a1fa3776deea8f7e8d9a6c30
Azra Becirevic <azra.becirevic1998@gmail.com>
2020-01-07 15:56:21 +0100

    izmjene: izgleda forme za login, validacija, validacije broja telefona, uslova za prikaz slika

--- Event_Attender/Event_Attender.Web/ViewModels/LoginVM.cs
+++ Event_Attender/Event_Attender.Web/ViewModels/LoginVM.cs
@@ -7,9 +7,11 @@ using System.Threading.Tasks;
 namespace Event_Attender.Web.ViewModels
 {
     public class LoginVM
-    {  /* [Required(ErrorMessage ="Obavezan unos")]*/
+    {   [Required(ErrorMessage ="Obavezan unos")]
+        [MaxLength(40)]
         public string username { get; set; }
-        //[Required(ErrorMessage ="Obavezan unos")]
+        [Required(ErrorMessage ="Obavezan unos")]
+        [MaxLength(25)]
         public string password { get; set; }
     }
 }
