2d580759f1e0af7a85605f5d3611b0aa9af82394
Azra Becirevic <azra.becirevic1998@gmail.com>
2020-01-07 15:31:34 +0100

    Izmjena uslova na priakzu slike eventa

--- Event_Attender/Event_Attender.Web/ViewModels/LoginVM.cs
+++ Event_Attender/Event_Attender.Web/ViewModels/LoginVM.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Event_Attender.Web.ViewModels
 {
     public class LoginVM
-    {
+    {  /* [Required(ErrorMessage ="Obavezan unos")]*/
         public string username { get; set; }
+        //[Required(ErrorMessage ="Obavezan unos")]
         public string password { get; set; }
     }
 }
