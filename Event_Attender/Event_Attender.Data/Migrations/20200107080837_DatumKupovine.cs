5c404be441898fcef5f6bb692bb828706692564f
Adil <adil@fit.ba>
2020-01-07 09:10:13 +0100

    dodata nova kolona datum kupovine i migracija

---
+++ Event_Attender/Event_Attender.Data/Migrations/20200107080837_DatumKupovine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Event_Attender.Data.Migrations
+{
+    public partial class DatumKupovine : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DatumKupovine",
+                table: "Karta",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DatumKupovine",
+                table: "Karta");
+        }
+    }
+}
