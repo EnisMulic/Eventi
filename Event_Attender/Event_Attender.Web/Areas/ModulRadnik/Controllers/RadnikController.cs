c39b93ac96e3d26370fe9c42d89f8a7ca9bf0955
Azra Becirevic <azra.becirevic1998@gmail.com>
2020-02-07 16:55:43 +0100

    Dodana Area Radnik, Controller, i redirekcija nakon prijave na Radnik/Index

---
+++ Event_Attender/Event_Attender.Web/Areas/ModulRadnik/Controllers/RadnikController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Event_Attender.Data.EF;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Event_Attender.Web.Areas.ModulRadnik.Controllers
+{
+    [Area("ModulRadnik")]
+    public class RadnikController : Controller
+    {
+
+        private readonly MojContext ctx;
+
+        public RadnikController(MojContext context)
+        {
+            ctx = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
