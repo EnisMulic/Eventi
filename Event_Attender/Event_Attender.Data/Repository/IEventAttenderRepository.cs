143bc1b8018c668100cad94a8e12449b165886bb
PancakeAlchemist <enis.mulic@edu.fit.ba>
2020-02-08 23:04:07 +0100

    [Remove] Nekoristeni controller

---
+++ Event_Attender/Event_Attender.Data/Repository/IEventAttenderRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Event_Attender.Data.Repository
+{
+    interface IEventAttenderRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(int id);
+        IQueryable<TEntity> GetAll();
+        void Add(TEntity entity);
+        void Remove(int id);
+        void Update(TEntity entity);
+    }
+}
