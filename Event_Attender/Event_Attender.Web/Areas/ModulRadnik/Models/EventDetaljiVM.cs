0ba3bddf3d696bad3b0c0d45e545f242a0c02b6e
Azra Becirevic <azra.becirevic1998@gmail.com>
2020-02-11 16:08:02 +0100

    Pregled detalja o eventu TaskID 86, UsersoryID 76

---
+++ Event_Attender/Event_Attender.Web/Areas/ModulRadnik/Models/EventDetaljiVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Event_Attender.Web.Areas.ModulRadnik.Models
+{
+    public class EventDetaljiVM
+    {
+        public int EventId { get; set; }
+        public string NazivEventa { get; set; }
+       
+        public List<TipProdaje> TipoviProdaje { get; set; }
+        public class TipProdaje
+        {
+            public int ProdajaTipId { get; set; }
+            public string TipKarte { get; set; }
+            public int UkupnoKarataTip { get; set; }
+            public int BrojProdatihKarataTip { get; set; }
+            public float CijenaTip { get; set; }
+            public int BrojPreostalihKarata { get; set; }
+            public float ZaradaOdTipa { get; set; }
+        }
+    }
+}
