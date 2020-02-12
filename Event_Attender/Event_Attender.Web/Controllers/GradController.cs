c88ca8978420aa433914353405e9ba0a2b2b55be
Azra Becirevic <azra.becirevic1998@gmail.com>
2020-02-12 10:06:12 +0100

    Uklanjanje nepotrebnih komentara

--- Event_Attender/Event_Attender.Web/Controllers/GradController.cs
+++ Event_Attender/Event_Attender.Web/Controllers/GradController.cs
@@ -19,26 +19,24 @@ namespace Event_Attender.Web.Controllers
         }
         public IActionResult Index()
         {
-            //using(MojContext ctx=new MojContext())
-            //{
+           
                 List<Grad> gradovi = ctx.Grad.Include(g=>g.Drzava).ToList();
                 ViewData["gradovi"] = gradovi;
-            //}
+           
             return View();
         }
         public IActionResult Dodaj()
         {   
             ViewData["grad"] = new Grad();
-            //using(MojContext ctx=new MojContext())
-            //{
+           
                ViewData["drzave"]= ctx.Drzava.ToList();
-            //}
+           
             return View();
         }
         public IActionResult Snimi(int id, int drzavaId, string naziv)
         {
             Grad g;
-           // MojContext ctx = new MojContext();
+          
             if (id == 0)
             {
                 g= new Grad();
@@ -57,21 +55,19 @@ namespace Event_Attender.Web.Controllers
         }
         public IActionResult Obrisi(int id)
         {
-            //using(MojContext ctx=new MojContext())
-            //{
+            
                 Grad g = ctx.Grad.Find(id);
                 ctx.Grad.Remove(g);
                 ctx.SaveChanges();
-            //}
+           
             return Redirect("/Grad/Index");
         }
         public IActionResult Uredi(int id)
         {
-            //using (MojContext ctx = new MojContext())
-            //{
+          
                 ViewData["grad"] =  ctx.Grad.Find(id);
                 ViewData["drzave"] = ctx.Drzava.ToList();
-            //}
+            
             return View("Dodaj");
         }
     }
