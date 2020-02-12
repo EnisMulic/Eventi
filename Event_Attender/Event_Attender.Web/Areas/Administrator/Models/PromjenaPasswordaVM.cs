de6c4a2e39bf0e5b3d7e8ef6182e55a5f72cc939
Enis <enis.mulic@edu.fit.ba>
2020-02-12 09:18:44 +0100

    [Add] Uredivanje podataka (i passworda) za administratora UserStoryID 89, TaskID 90 & 91

--- Event_Attender/Event_Attender.Web/Areas/Administrator/Models/PromjenaPasswordaVM.cs
+++ Event_Attender/Event_Attender.Web/Areas/Administrator/Models/PromjenaPasswordaVM.cs
@@ -7,36 +7,10 @@ using System.Threading.Tasks;
 
 namespace Event_Attender.Web.Areas.Administrator.Models
 {
-    public class AdministratorVM
+    public class PromjenaPasswordaVM
     {
+        //Admin id
         public int Id { get; set; }
-
-        [Required(ErrorMessage = "Obavezno polje")]
-        [MaxLength(20)]
-        public string Ime { get; set; }
-
-        [Required(ErrorMessage = "Obavezno polje")]
-        [MaxLength(30)]
-        public string Prezime { get; set; }
-
-        [Required(ErrorMessage = "Obavezno polje")]
-        [RegularExpression(@"\+[0-9]{3}[\s][0-9]{2}[\s][0-9]{3}[\s][0-9]{3}", 
-                ErrorMessage = "U formatu +387 xx xxx xxx")]
-        public string Telefon { get; set; }
-
-        [Required]
-        [Remote(action: "IsEmailUnique", controller: "Administrator", areaName: "Administrator",
-                AdditionalFields = "LogPodaciId",
-                ErrorMessage = "Email Vec postoji")]
-        public string Email { get; set; }
-
-        [Required]
-        [Remote(action: "IsUsernameUnique", controller: "Administrator", areaName: "Administrator",
-                AdditionalFields = "LogPodaciId",
-                ErrorMessage = "Username Vec postoji")]
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Grad { get; set; }
         public int LogPodaciId { get; set; }
 
         [Required(ErrorMessage = "Obavezno polje")]
