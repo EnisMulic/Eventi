172f6bd343572574623905519754c97e8a84b80c
Enis <enis.mulic@edu.fit.ba>
2020-02-24 13:14:58 +0100

    [Fix] signalR chat

--- Event_Attender/Event_Attender.Web/Areas/Administrator/Controllers/AdministratorController.cs
+++ Event_Attender/Event_Attender.Web/Areas/Administrator/Controllers/AdministratorController.cs
@@ -102,48 +102,8 @@ namespace Event_Attender.Web.Areas.Administrator.Controllers
             LogPodaci.Password = model.NewPassword;
             ctx.SaveChanges();
             return Redirect("AdminProfil?id=" + model.Id);
-        }
+        }      
 
-        public IActionResult Poruke()
-        {
-            var Log = HttpContext.GetLogiraniUser();
-            var Osoba = uow.OsobaRepository.GetAll()
-                .Where(i => i.LogPodaciId == Log.Id)
-                .SingleOrDefault();
-
-            var model = new ChatVM
-            {
-                AdministratorId = Osoba.Id,
-                Username = Log.Username,
-                Rows = uow.ChatPorukeRepository.GetAll()
-                    .Select
-                    (
-                        i => new ChatVM.Row
-                        {
-                            AutorId = i.OsobaId,
-                            Autor = i.Osoba.LogPodaci.Username,
-                            ChatPorukaId = i.Id,
-                            Poruka = i.Poruka,
-                            Kreirana = i.Kreirana.ToString("dd/MM/yyyy hh:mm")
-
-                        }
-                    )
-                    .ToList()
-            };
-            return View(model);
-        }
-
-        public IActionResult SnimiPoruku(int userId, string poruka)
-        {
-            var Poruka = new ChatPoruke
-            {
-                OsobaId = userId,
-                Poruka = poruka,
-                Kreirana = DateTime.Now
-            };
-            uow.ChatPorukeRepository.Add(Poruka);
-            return Redirect("Poruke");
-        }
 
         public bool IsUsernameUnique(string Username, int LogPodaciId)
         {
@@ -170,5 +130,30 @@ namespace Event_Attender.Web.Areas.Administrator.Controllers
 
         public bool MatchNewPassword(string NewPasswordConfirmed, string NewPassword) =>
             NewPasswordConfirmed == NewPassword;
+
+        public IActionResult Poruke()
+        {
+            
+            var model = new ChatVM
+            {
+                Username = HttpContext.GetLogiraniUser().Username,
+                AdministratorId = HttpContext.GetLogiraniUser().Id,
+                Rows = uow.ChatPorukeRepository.GetAll()
+                    .Select
+                    (
+                        i => new ChatVM.Row
+                        {
+                            ChatPorukaId = i.Id,
+                            AutorId = i.Osoba.Id,
+                            Autor = i.Osoba.LogPodaci.Username,
+                            Poruka = i.Poruka,
+                            Kreirana = i.Kreirana.ToString("dd/MM/yyyy HH:mm")
+                        }
+                    )
+                    .ToList()
+            };
+
+            return View(model);
+        }
     }
 }
