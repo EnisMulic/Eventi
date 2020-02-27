7cec6d3c0be410bd3bead8f6b35e867be8a65b91
Lizarazu <zinedin.mezit.98@gmail.com>
2020-02-27 11:17:16 +0100

    Dodane nestandardne funkcionalnosti.Dodane preostale standardne funckionalnosti za Radnika i Organizatora

---
+++ Event_Attender/Event_Attender.Web/Areas/OrganizatorModul/ViewModels/EventUrediFormVM.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Event_Attender.Web.Areas.OrganizatorModul.ViewModels
+{
+
+    public class EventUrediFormVM
+    {
+        private Array vrijednostiKategorije = Enum.GetValues(typeof(Kategorija));
+
+        public int EventId { get; set; }
+        public string NazivEventaOd { get; set; }
+        public string OpisEventaOd { get; set; }
+        public string VrijemeOdrzavanjaOd { get; set; }
+      
+        public int KategorijaOdabir { get; set; }
+        public List<SelectListItem> Kategorije { get; set; } = new List<SelectListItem>();
+
+        public int SponzorOdabir { get; set; }
+        public List<SelectListItem> Sponzori { get; set; } = new List<SelectListItem>();
+
+        public EventUrediFormVM()
+        {
+            foreach(var item in vrijednostiKategorije)
+            {
+                Kategorije.Add(new SelectListItem
+                {
+                    Value = ((int)item).ToString(),
+                    Text = Enum.GetName(typeof(Kategorija), item)
+                });
+            }
+        }
+      
+    }
+}
