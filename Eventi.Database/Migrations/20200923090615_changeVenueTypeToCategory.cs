36bd00e1174f21f3b77f44a0e8349d3a7b999a92
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-23 11:09:04 +0200

    Change venue type to venue category

---
+++ Eventi.Database/Migrations/20200923090615_changeVenueTypeToCategory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Eventi.Database.Migrations
+{
+    public partial class changeVenueTypeToCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VenueType",
+                table: "Venues");
+
+            migrationBuilder.AddColumn<int>(
+                name: "VenueCategory",
+                table: "Venues",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VenueCategory",
+                table: "Venues");
+
+            migrationBuilder.AddColumn<int>(
+                name: "VenueType",
+                table: "Venues",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- Eventi.Domain/Venue.cs
+++ Eventi.Domain/Venue.cs
@@ -1,12 +1,13 @@
-﻿namespace Eventi.Domain
+﻿using Eventi.Common;
+
+namespace Eventi.Domain
 {
-    public enum VenueType { Sala, Dvorana, Stadion }
     public class Venue
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
-        public VenueType VenueType { get; set; }
+        public VenueCategory VenueCategory { get; set; }
         public int CityID { get; set; }
         public City City { get; set; }
     }
