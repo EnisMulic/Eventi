e10ac74f8de1c4766d5a6fa14ed4a0aa35715ecb
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-24 09:11:13 +0200

    Update for renamed classes

--- Eventi.Contracts/V1/Requests/LoginRequest.cs
+++ Eventi.Contracts/V1/Requests/LoginRequest.cs
@@ -1,9 +1,8 @@
 ﻿namespace Eventi.Contracts.V1.Requests
 {
-    public class AccountRegistrationRequest
+    public class LoginRequest
     {
         public string Username { get; set; }
-        public string Email { get; set; }
         public string Password { get; set; }
     }
 }
