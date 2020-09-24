a38057ddddb01796ad24db80b070325bbfcf19fb
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-24 09:11:13 +0200

    Update for renamed classes

--- Eventi.Contracts/V1/ApiRoutes.cs
+++ Eventi.Contracts/V1/ApiRoutes.cs
@@ -10,23 +10,26 @@
         public static class Auth
         {
             public const string Register = Base + "/Account/Register";
-
             public const string Login = Base + "/Account/Login";
-
             public const string Refresh = Base + "/Account/Refresh";
         }
 
         public static class User
         {
             public const string Get = Base + "/User";
-
             public const string GetById = Base + "/User";
-
             public const string Post = Base + "/User";
-
             public const string Put = Base + "/User";
-
             public const string Delete = Base + "/User";
+        }
+
+        public static class City
+        {
+            public const string Get = Base + "/City";
+            public const string GetById = Base + "/City";
+            public const string Post = Base + "/City";
+            public const string Put = Base + "/City";
+            public const string Delete = Base + "/City";
         }
     }
 }
--- Eventi.WebAPI.IntegrationTests/IntegrationTest.cs
+++ Eventi.WebAPI.IntegrationTests/IntegrationTest.cs
@@ -53,7 +53,7 @@ namespace Eventi.WebAPI.IntegrationTests
 
         private async Task<string> GetJwtAsync()
         {
-            var response = await _httpClient.PostAsJsonAsync(ApiRoutes.Auth.Register, new AccountRegistrationRequest
+            var response = await _httpClient.PostAsJsonAsync(ApiRoutes.Auth.Register, new RegistrationRequest
             {
                 Username = "Integrator",
                 Email = "test2@integration.com",
