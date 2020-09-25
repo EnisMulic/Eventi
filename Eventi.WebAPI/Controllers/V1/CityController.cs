8c499cf612543c038aa3b0729f3de1bf04009e2e
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-25 23:18:47 +0200

    Fix dependency injection for controllers

--- Eventi.WebAPI/Controllers/V1/CityController.cs
+++ Eventi.WebAPI/Controllers/V1/CityController.cs
@@ -2,13 +2,12 @@
 using Eventi.Contracts.V1.Requests;
 using Eventi.Contracts.V1.Responses;
 using Eventi.Core.Interfaces;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Eventi.WebAPI.Controllers.V1
 {
     public class CityController : CRUDController<CityResponse, CitySearchRequest, CityUpsertRequest, CityUpsertRequest>
     {
-        public CityController(ICRUDService<CityResponse, CitySearchRequest, CityUpsertRequest, CityUpsertRequest> service, IMapper mapper) : base(service, mapper)
+        public CityController(ICityService service, IMapper mapper) : base(service, mapper)
         {
         }
     }
--- Eventi.WebAPI/Controllers/V1/CountryController.cs
+++ Eventi.WebAPI/Controllers/V1/CountryController.cs
@@ -2,13 +2,12 @@
 using Eventi.Contracts.V1.Requests;
 using Eventi.Contracts.V1.Responses;
 using Eventi.Core.Interfaces;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Eventi.WebAPI.Controllers.V1
 {
     public class CountryController : CRUDController<CountryResponse, CountrySearchRequest, CountryUpsertRequest, CountryUpsertRequest>
     {
-        public CountryController(ICRUDService<CountryResponse, CountrySearchRequest, CountryUpsertRequest, CountryUpsertRequest> service, IMapper mapper) : base(service, mapper)
+        public CountryController(ICountryService service, IMapper mapper) : base(service, mapper)
         {
         }
     }
