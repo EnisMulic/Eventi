d73105ef3ffeb33219873eeec8e43725ffd7a238
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-25 23:18:47 +0200

    Fix dependency injection for controllers

--- Eventi.WebAPI/Controllers/V1/VenueController.cs
+++ Eventi.WebAPI/Controllers/V1/VenueController.cs
@@ -8,7 +8,7 @@ namespace Eventi.WebAPI.Controllers.V1
 {
     public class VenueController : CRUDController<VenueResponse, VenueSearchRequest, VenueUpsertRequest, VenueUpsertRequest>
     {
-        public VenueController(ICRUDService<VenueResponse, VenueSearchRequest, VenueUpsertRequest, VenueUpsertRequest> service, IMapper mapper) : base(service, mapper)
+        public VenueController(IVenueService service, IMapper mapper) : base(service, mapper)
         {
         }
     }
