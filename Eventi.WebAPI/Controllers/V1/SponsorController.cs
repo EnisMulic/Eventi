eb0e747a090b52e9f242010474d67eeee871d290
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-26 08:53:48 +0200

    Add Sponsor Service and Controller

---
+++ Eventi.WebAPI/Controllers/V1/SponsorController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Eventi.Contracts.V1.Requests;
+using Eventi.Contracts.V1.Responses;
+using Eventi.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventi.WebAPI.Controllers.V1
+{
+    [ApiController]
+    public class SponsorController : CRUDController<SponsorResponse, SponsorSearchRequest, SponsorUpsertRequest, SponsorUpsertRequest>
+    {
+        public SponsorController(ICRUDService<SponsorResponse, SponsorSearchRequest, SponsorUpsertRequest, SponsorUpsertRequest> service, IMapper mapper) 
+            : base(service, mapper)
+        {
+        }
+    }
+}
