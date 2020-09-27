82a20b213aa02966cf072faa83e1725b0102821b
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-27 20:30:50 +0200

    Implement County Endpoint

--- Eventi.Core/Interfaces/ICountryService.cs
+++ Eventi.Core/Interfaces/ICountryService.cs
@@ -1,9 +1,12 @@
 ﻿using Eventi.Contracts.V1.Requests;
 using Eventi.Contracts.V1.Responses;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Eventi.Core.Interfaces
 {
     public interface ICountryService : ICRUDService<CountryResponse, CountrySearchRequest, CountryUpsertRequest, CountryUpsertRequest>
     {
+        public Task<List<CityResponse>> GetCityAsync(int id);
     }
 }
