36a278e366ea2579c8cd0d3b07df05d67e3c9d67
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-28 10:33:29 +0200

    Implement endpoint city

--- Eventi.Core/Interfaces/ICityService.cs
+++ Eventi.Core/Interfaces/ICityService.cs
@@ -1,9 +1,13 @@
 ﻿using Eventi.Contracts.V1.Requests;
 using Eventi.Contracts.V1.Responses;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Eventi.Core.Interfaces
 {
     public interface ICityService : ICRUDService<CityResponse, CitySearchRequest, CityUpsertRequest, CityUpsertRequest>
     {
+        public Task<PagedResponse<VenueResponse>> GetVenueAsync(int id, PaginationQuery pagination);
+        public Task<PagedResponse<EventResponse>> GetEventAsync(int id, PaginationQuery pagination);
     }
 }
