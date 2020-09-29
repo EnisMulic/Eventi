3a1749f64a1af5c5df6c9cd40921761af6edd451
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-29 11:13:46 +0200

    Implement Get, GetById, Update for Client endpoint

--- Eventi.Contracts/V1/Responses/ClientResponse.cs
+++ Eventi.Contracts/V1/Responses/ClientResponse.cs
@@ -1,13 +1,17 @@
-﻿namespace Eventi.Contracts.V1.Requests
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventi.Contracts.V1.Responses
 {
-    public class ClientUpdateRequest
+    public class ClientResponse
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
-        public string CreditCardNumber { get; set; }
+        public string PhoneNumber { get; set; }
         public string Image { get; set; }
     }
 }
