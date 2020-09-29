554e7e339010b7335916d6fe7d6af564f1e94e27
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-29 10:11:15 +0200

    Fix methods to work with integer ids

--- Eventi.Core/Interfaces/ICRUDService.cs
+++ Eventi.Core/Interfaces/ICRUDService.cs
@@ -5,7 +5,7 @@ namespace Eventi.Core.Interfaces
     public interface ICRUDService<T, TSearch, TInsert, TUpdate> : IBaseService<T, TSearch>
     {
         Task<T> Insert(TInsert request);
-        Task<T> Update(string id, TUpdate request);
-        Task<bool> Delete(string id);
+        Task<T> Update(int id, TUpdate request);
+        Task<bool> Delete(int id);
     }
 }
