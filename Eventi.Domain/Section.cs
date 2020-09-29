c05953f644aebf51686f94d7bf0caa67f4214faa
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-29 20:16:50 +0200

    Update domain for ticketing system

---
+++ Eventi.Domain/Section.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventi.Domain
+{
+    public class Section
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int VenueID { get; set; }
+        public Venue Venue { get; set; }
+    }
+}
