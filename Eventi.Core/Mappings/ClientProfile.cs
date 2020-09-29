36491e120174bbe5ffd25d9d451a978394617e2b
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-29 11:13:46 +0200

    Implement Get, GetById, Update for Client endpoint

--- Eventi.Core/Mappings/ClientProfile.cs
+++ Eventi.Core/Mappings/ClientProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Eventi.Contracts.V1.Requests;
+using Eventi.Contracts.V1.Responses;
 using Eventi.Domain;
 
 namespace Eventi.Core.Mappings
@@ -9,6 +10,13 @@ namespace Eventi.Core.Mappings
         public ClientProfile()
         {
             CreateMap<ClientRegistrationRequest, Client>();
+            CreateMap<ClientUpdateRequest, Client>();
+            CreateMap<Client, ClientResponse>()
+                .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Person.Account.Username))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Person.Account.Email))
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.Person.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.Person.LastName))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.Person.PhoneNumber));
         }
     }
 }
---
+++ Eventi.WebAPI/Controllers/V1/ClientController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Eventi.Contracts.V1.Requests;
+using Eventi.Contracts.V1.Responses;
+using Eventi.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventi.WebAPI.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : CRUDController<ClientResponse, ClientSearchRequest, object, ClientUpdateRequest>
+    {
+        public ClientController(IClientService service, IMapper mapper) : base(service, mapper)
+        {
+        }
+    }
+}
