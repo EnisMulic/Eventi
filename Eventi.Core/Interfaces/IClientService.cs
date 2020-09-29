f02aae6eb9d724897354fb1851d2dfa693594e36
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-29 11:13:46 +0200

    Implement Get, GetById, Update for Client endpoint

---
+++ Eventi.Core/Interfaces/IClientService.cs
@@ -0,0 +1,11 @@
+﻿using Eventi.Contracts.V1.Requests;
+using Eventi.Contracts.V1.Responses;
+using System.Threading.Tasks;
+
+namespace Eventi.Core.Interfaces
+{
+    public interface IClientService : ICRUDService<ClientResponse, ClientSearchRequest, object, ClientUpdateRequest>
+    {
+        public Task<PagedResponse<EventResponse>> GetEvents(int ID, EventSearchRequest search, PaginationQuery pagination);
+    }
+}
