b7811facbae138ff8a34ea95dd4d78849088ca24
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-30 08:46:52 +0200

    Implement Client endpoint, resolves #13

--- Eventi.Core/Interfaces/IClientService.cs
+++ Eventi.Core/Interfaces/IClientService.cs
@@ -1,11 +1,12 @@
 ﻿using Eventi.Contracts.V1.Requests;
 using Eventi.Contracts.V1.Responses;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Eventi.Core.Interfaces
 {
     public interface IClientService : ICRUDService<ClientResponse, ClientSearchRequest, object, ClientUpdateRequest>
     {
-        public Task<PagedResponse<EventResponse>> GetEvents(int ID, EventSearchRequest search, PaginationQuery pagination);
+        public Task<List<EventResponse>> GetEvents(int id);
     }
 }
