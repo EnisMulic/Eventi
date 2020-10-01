95fd58297e2cb37752074905a55612d8c2a5e750
Enis Mulić <enis.s.mulic@gmail.com>
2020-10-01 15:34:13 +0200

    Preperations for data seeding

---
+++ Eventi.Database/EventiContextData.cs
@@ -0,0 +1,41 @@
+﻿using Eventi.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Eventi.Database
+{
+    public partial class EventiContext
+    {
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>()
+                .HasData
+                (
+                    new List<Country>()
+                    {
+                        new Country { ID = 1, Name = "Bosnia and Herzegovina"},
+                        new Country { ID = 2, Name = "Croatia"},
+                        new Country { ID = 3, Name = "Serbia"}
+                    }
+                );
+
+            modelBuilder.Entity<City>()
+                .HasData
+                (
+                    new List<City>()
+                    {
+                        new City { ID = 1, CountryID = 1, Name = "Sarajevo"},
+                        new City { ID = 2, CountryID = 1, Name = "Mostar"},
+                        new City { ID = 3, CountryID = 1, Name = "Zenica"},
+                        new City { ID = 4, CountryID = 1, Name = "Tuzla"},
+                        new City { ID = 5, CountryID = 1, Name = "Banja Luka"},
+                        new City { ID = 6, CountryID = 1, Name = "Bihać"},
+                        new City { ID = 7, CountryID = 2, Name = "Zagreb"},
+                        new City { ID = 8, CountryID = 2, Name = "Split"},
+                        new City { ID = 9, CountryID = 3, Name = "Beograd"},
+                        new City { ID = 10, CountryID = 3, Name = "Novi Sad"},
+                    }
+                );
+        }
+    }
+}
---
+++ Eventi.Database/Migrations/20201001114815_AddSeedForCountriesAndCities.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Eventi.Database.Migrations
+{
+    public partial class AddSeedForCountriesAndCities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 1, "Bosnia and Herzegovina" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 2, "Croatia" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 3, "Serbia" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "ID", "CountryID", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Sarajevo" },
+                    { 2, 1, "Mostar" },
+                    { 3, 1, "Zenica" },
+                    { 4, 1, "Tuzla" },
+                    { 5, 1, "Banja Luka" },
+                    { 6, 1, "Bihać" },
+                    { 7, 2, "Zagreb" },
+                    { 8, 2, "Split" },
+                    { 9, 3, "Beograd" },
+                    { 10, 3, "Novi Sad" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "ID",
+                keyValue: 3);
+        }
+    }
+}
