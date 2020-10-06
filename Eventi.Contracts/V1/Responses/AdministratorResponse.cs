413b27065f93dcb8d8fc0f792267f332dd4f9ee0
Enis Mulić <enis.s.mulic@gmail.com>
2020-10-06 23:28:30 +0200

    Cleanup solution

--- Eventi.Contracts/V1/Responses/AdministratorResponse.cs
+++ Eventi.Contracts/V1/Responses/AdministratorResponse.cs
@@ -6,8 +6,5 @@
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Image { get; set; }
     }
 }
