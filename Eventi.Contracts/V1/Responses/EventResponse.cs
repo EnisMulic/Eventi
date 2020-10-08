ab27ffb6b6ec64f4ce2b141107129c0e91b796d5
Enis Mulić <enis.s.mulic@gmail.com>
2020-10-08 19:46:47 +0200

    Add ID to EventResponse

--- Eventi.Contracts/V1/Responses/EventResponse.cs
+++ Eventi.Contracts/V1/Responses/EventResponse.cs
@@ -5,6 +5,7 @@ namespace Eventi.Contracts.V1.Responses
 {
     public class EventResponse
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime Start { get; set; }
