a951076fa268795714c5f2cc7f72c9b62e8fad00
Enis Mulić <enis.s.mulic@gmail.com>
2020-10-08 21:06:42 +0200

    Seed events

--- Eventi.Database/Migrations/20201008190006_seedEvent.Designer.cs
+++ Eventi.Database/Migrations/20201008190006_seedEvent.Designer.cs
@@ -4,14 +4,16 @@ using Eventi.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Eventi.Database.Migrations
 {
     [DbContext(typeof(EventiContext))]
-    partial class EventiContextModelSnapshot : ModelSnapshot
+    [Migration("20201008190006_seedEvent")]
+    partial class seedEvent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
