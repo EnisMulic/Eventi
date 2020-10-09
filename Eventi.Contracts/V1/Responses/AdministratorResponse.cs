92d5bfdeab40184da6fb15cd7319a6ae701f887f
Enis Mulić <enis.s.mulic@gmail.com>
2020-10-09 19:20:36 +0200

    Add IDs to responses for client, admin, organizer

--- Eventi.Contracts/V1/Responses/AdministratorResponse.cs
+++ Eventi.Contracts/V1/Responses/AdministratorResponse.cs
@@ -2,6 +2,7 @@
 {
     public class AdministratorResponse
     {
+        public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
--- Eventi.Contracts/V1/Responses/OrganizerResponse.cs
+++ Eventi.Contracts/V1/Responses/OrganizerResponse.cs
@@ -2,6 +2,7 @@
 {
     public class OrganizerResponse
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Username { get; set; }
