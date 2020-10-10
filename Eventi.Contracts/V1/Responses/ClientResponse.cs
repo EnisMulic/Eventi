ba496a08e419d4888e6c74fb2ba7c16da331e0a1
Enis Mulić <enis.s.mulic@gmail.com>
2020-10-10 15:45:24 +0200

    Update response models to return account id

--- Eventi.Contracts/V1/Responses/ClientResponse.cs
+++ Eventi.Contracts/V1/Responses/ClientResponse.cs
@@ -7,6 +7,7 @@ namespace Eventi.Contracts.V1.Responses
     public class ClientResponse
     {
         public int ID { get; set; }
+        public int AccountID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
--- Eventi.Core/Mappings/ClientProfile.cs
+++ Eventi.Core/Mappings/ClientProfile.cs
@@ -16,7 +16,8 @@ namespace Eventi.Core.Mappings
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Person.Account.Email))
                 .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.Person.FirstName))
                 .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.Person.LastName))
-                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.Person.PhoneNumber));
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.Person.PhoneNumber))
+                .ForMember(dest => dest.AccountID, opt => opt.MapFrom(src => src.Person.AccountID));
         }
     }
 }
