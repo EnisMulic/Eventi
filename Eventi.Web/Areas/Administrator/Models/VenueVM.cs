04e9b28745904e50bd8457de341d91656ec4be28
Enis Mulić <enis.s.mulic@gmail.com>
2020-10-12 21:48:45 +0200

    Add CRUD for Venue

---
+++ Eventi.Web/Areas/Administrator/Models/VenueVM.cs
@@ -0,0 +1,23 @@
+﻿using Eventi.Common;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Eventi.Web.Areas.Administrator.Models
+{
+    public class VenueVM
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public VenueCategory VenueCategory { get; set; }
+        [Required]
+        public int CityID { get; set; }
+        public string CityName { get; set; }
+
+        public List<SelectListItem> Cities { get; set; }
+    }
+}
