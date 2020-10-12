1b23fc4b5bf815a7e2ff27ad7f7189006f249467
Enis Mulić <enis.s.mulic@gmail.com>
2020-10-12 23:16:55 +0200

    Add CRUD for Organizer

--- Eventi.Web/Areas/Administrator/Controllers/OrganizerController.cs
+++ Eventi.Web/Areas/Administrator/Controllers/OrganizerController.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Eventi.Common;
-using Eventi.Data.EF;
+using Eventi.Contracts.V1.Requests;
 using Eventi.Data.Models;
-using Eventi.Data.Repository;
+using Eventi.Sdk;
 using Eventi.Web.Areas.Administrator.Models;
 using Eventi.Web.Helper;
 using Microsoft.AspNetCore.Mvc;
@@ -18,27 +17,26 @@ namespace Eventi.Web.Areas.Administrator.Controllers
     [Area("Administrator")]
     public class OrganizerController : Controller
     {
-        private readonly MojContext ctx;
-        private readonly EventAttenderUnitOfWork uow;
-        public OrganizerController(MojContext context)
+        private readonly IEventiApi _eventiApi;
+        public OrganizerController(IEventiApi eventiApi)
         {
-            ctx = context;
-            uow = new EventAttenderUnitOfWork(ctx);
+            _eventiApi = eventiApi;
         }
-        public IActionResult OrganizerList()
+        public async Task<IActionResult> OrganizerList()
         {
-            var model = uow.OrganizatorRepository.GetAll()
+            var response = await _eventiApi.GetOrganizerAsync();
+            var model = response.Content.Data
                 .Select
                 (
-                    i => new OrganizatorVM
+                    i => new OrganizerVM
                     {
-                        Id = i.Id,
-                        Naziv = i.Naziv,
-                        Telefon = i.Telefon,
-                        GradNaziv = i.Grad.Naziv,
-                        Username = i.LogPodaci.Username,
-                        Email = i.LogPodaci.Email
-
+                        ID = i.ID,
+                        AccountID = i.AccountID,
+                        Name = i.Name,
+                        PhoneNumber = i.PhoneNumber,
+                        CityID = i.CityID,
+                        Username = i.Username,
+                        Email = i.Email
                     }
                 )
                 .ToList();
@@ -46,143 +44,84 @@ namespace Eventi.Web.Areas.Administrator.Controllers
             return View(model);
         }
 
-        public IActionResult OrganizerUkloni(int Id)
+        public async Task<IActionResult> OrganizerRemove(int ID)
         {
-            var item = uow.OrganizatorRepository.Get(Id);
-
-            if (item != null)
-            {
-
-                uow.OrganizatorRepository.Remove(Id);
-            }
-
-            return Redirect("Index");
+            await _eventiApi.DeleteOrganizerAsync(ID);
+            return Redirect("/Administrator/Home/Index");
         }
 
-        public IActionResult OrganizerInfo(int Id)
+        public async Task<IActionResult> OrganizerDetails(int ID)
         {
-            var model = uow.OrganizatorRepository.GetAll()
-                .Select
-                (
-                    i => new OrganizatorVM
-                    {
-                        Id = i.Id,
-                        Naziv = i.Naziv,
-                        Telefon = i.Telefon,
-                        GradId = i.Grad.Id,
-                        GradNaziv = i.Grad.Naziv,
-                        Username = i.LogPodaci.Username,
-                        Email = i.LogPodaci.Email,
-                        Password = i.LogPodaci.Password
-                    }
-                )
-                .Where(i => i.Id == Id)
-                .SingleOrDefault();
-
+            var response = await _eventiApi.GetOrganizerAsync(ID);
+            var entity = response.Content;
+            var model = new OrganizerVM()
+            {
+                ID = entity.ID,
+                AccountID = entity.AccountID,
+                Name = entity.Name,
+                Username = entity.Username,
+                Email = entity.Email,
+                PhoneNumber = entity.PhoneNumber,
+                CityID = entity.CityID
+            };
             return View(model);
         }
 
-        public IActionResult OrganizerUredi(int Id)
+        public async Task<IActionResult> OrganizerEdit(int ID)
         {
-            var model = uow.OrganizatorRepository.GetAll()
-                .Select
-                (
-                    i => new OrganizatorVM
-                    {
-                        Id = i.Id,
-                        Naziv = i.Naziv,
-                        Telefon = i.Telefon,
-                        GradId = i.Grad.Id,
-                        GradNaziv = i.Grad.Naziv,
-                        Username = i.LogPodaci.Username,
-                        Email = i.LogPodaci.Email,
-                        Password = i.LogPodaci.Password,
-                        LogPodaciId = i.LogPodaci.Id
-                    }
-                )
-                .Where(i => i.Id == Id)
-                .SingleOrDefault();
-
-            model.Gradovi = uow.GradRepository.GetAll().Select(
-                i => new SelectListItem(i.Naziv, i.Id.ToString())).ToList();
-
-            return View(model);
-
-
-        }
-
-        public IActionResult OrganizerSnimi(OrganizatorVM model)
-        {
-            var item = uow.OrganizatorRepository.GetAll()
-                .Include(i => i.LogPodaci)
-                .Where(i => i.Id == model.Id)
-                .FirstOrDefault();
-
-            if (item.LogPodaciId == null)
+            var response = await _eventiApi.GetOrganizerAsync(ID);
+            var entity = response.Content;
+            var model = new OrganizerVM()
             {
-                item.LogPodaci = new LogPodaci();
-            }
-
-            item.Naziv = model.Naziv;
-            item.LogPodaci.Username = model.Username;
-            item.LogPodaci.Email = model.Email;
-            item.LogPodaci.Password = model.Password;
-            item.GradId = model.GradId;
-            item.Telefon = model.Telefon;
-
-
-            ctx.SaveChanges();
-
-            return Redirect("Index");
-        }
-
-        public IActionResult OrganizerDodaj()
-        {
-            var model = new OrganizatorVM
-            {
-                Gradovi = uow.GradRepository.GetAll().Select(
-                    i => new SelectListItem(i.Naziv, i.Id.ToString())).ToList()
+                ID = entity.ID,
+                AccountID = entity.AccountID,
+                Name = entity.Name,
+                Username = entity.Username,
+                Email = entity.Email,
+                PhoneNumber = entity.PhoneNumber,
+                CityID = entity.CityID
             };
-
             return View(model);
         }
 
-
-
-        public IActionResult OrganizerDodajSnimi(OrganizatorVM model)
+        public async Task<IActionResult> OrganizerSave(OrganizerVM model)
         {
-
-            var log = new LogPodaci
+            if(model.ID == 0)
             {
-                Username = model.Username,
-                Email = model.Email,
-                Password = model.Password
-            };
+                var insertRequest = new OrganizerInsertRequest()
+                {
+                    Name = model.Name,
+                    CityID = model.CityID,
+                    PhoneNumber = model.PhoneNumber
+                };
 
-            uow.LogPodaciRepository.Add(log);
-
-
-            var item = new Organizator
-            {
-                Naziv = model.Naziv,
-                Telefon = model.Telefon,
-                GradId = model.GradId,
-                LogPodaci = log
-            };
-
-            try
-            {
-                uow.OrganizatorRepository.Add(item);
+                await _eventiApi.CreateOrganizerAsync(insertRequest);
             }
-            catch //(Exception e)
+            else
             {
-                //Console.WriteLine("{0} Exception caught.", e);
+                var updateRequest = new OrganizerUpdateRequest()
+                {
+                    Name = model.Name,
+                    CityID = model.CityID,
+                    PhoneNumber = model.PhoneNumber
+                };
+
+                await _eventiApi.UpdateOrganizerAsync(model.ID, updateRequest);
             }
 
+            return Redirect("/Administrator/Home/Index");
+        }
 
+        public async Task<IActionResult> OrganizerCreate()
+        {
+            var response = await _eventiApi.GetCityAsync();
+            var Cities = response.Content.Data;
+            var model = new OrganizerVM
+            {
+                Cities = Cities.Select(i => new SelectListItem(i.Name, i.ID.ToString())).ToList()
+            };
 
-
-            return Redirect("Index");
+            return View(model);
         }
     }
 }
